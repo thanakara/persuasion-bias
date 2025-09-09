3a16a85e8dd7336fafd13aa59e2db6acf4041eba
thanakara <thkaranikolopoulos@gmail.com>
2025-09-09 18:25:58 +0300

    New @joins decorator for retriever Callables.

--- src/persuasion_bias/utils/wrappers.py
+++ src/persuasion_bias/utils/wrappers.py
@@ -1,3 +1,6 @@
+from functools import wraps
+from typing import Callable
+
 from IPython.display import Markdown, display
 from langchain_core.documents import Document
 from langchain_core.messages import HumanMessage
@@ -12,6 +15,20 @@ def join_documents(documents: list[Document]) -> str:
         result.append(document.page_content)
 
     return "\n\n".join(result)
+
+
+# Decorator over Callables that invoke the retriever and return List[Document]
+def joins(func: Callable) -> Callable:
+    @wraps("func")
+    def _merge(*args, **kwargs) -> str:
+        retrieval = func(*args, **kwargs)
+        iterable = []
+
+        for document in retrieval:
+            iterable.append(document.page_content)
+        return "\n---------\n".join(iterable)
+
+    return _merge
 
 
 def get_response_content(agent: CompiledStateGraph, argument: str) -> None:
